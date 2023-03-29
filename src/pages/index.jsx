2563a07ce1f2d9d435c0027c1b08d66e33dfa9ce
NurjahonErgashev <lobarqochqorova18@gmail.com>
2023-03-29 16:37:20 +0500

    nmadr

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,7 @@
-import Card from "@/components/Card";
 import Link from "next/link";
 import React from "react";
 import styles from "./index.module.scss";
-
+import { Card, Col, Row, Button, Text } from "@nextui-org/react";
 function Home({ data }) {
   console.log(data);
   return (
@@ -11,8 +10,64 @@ function Home({ data }) {
       <br />
       <div className={styles.animes}>
         {data.data.map((anime) => (
-          <Link href={`/all/${encodeURI(anime.title)}`}>
-            <Card data={anime} {...anime} />
+          <Link href={`/all/${anime.mal_id}`}>
+            <Card css={{ w: "100%", h: "400px" }}>
+              <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+                <Col>
+                  <Text
+                    size={12}
+                    weight="bold"
+                    transform="uppercase"
+                    color="#ffffffAA"
+                  >
+                    {anime.year}
+                  </Text>
+                </Col>
+              </Card.Header>
+              <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  src={anime.images.jpg.large_image_url}
+                  width="100%"
+                  height="100%"
+                  objectFit="cover"
+                  alt="Card example background"
+                />
+              </Card.Body>
+              <Card.Footer
+                isBlurred
+                css={{
+                  position: "absolute",
+                  bgBlur: "#ffffff66",
+                  borderTop:
+                    "$borderWeights$light solid rgba(255, 255, 255, 0.2)",
+                  bottom: 0,
+                  zIndex: 1,
+                }}
+              >
+                <Row>
+                  <Col>
+                   
+                    <Text color="#000" size={12}>
+                      {anime.title}
+                    </Text>
+                  </Col>
+                  <Col>
+                    <Row justify="flex-end">
+                      <Button flat auto rounded color="secondary">
+                        <Text
+                          css={{ color: "inherit" }}
+                          size={12}
+                          weight="bold"
+                          transform="uppercase"
+                        >
+                          more 
+                        </Text>
+                      </Button>
+                    </Row>
+                  </Col>
+                </Row>
+              </Card.Footer>
+            </Card>
           </Link>
         ))}
       </div>
